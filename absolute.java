2d1d78ea8122d67c12456587d33d8589bbe4628a
marrerap <andrew.marrero@outlook.com>
2021-12-22 14:24:47 -0600

    added aboslute example

---
+++ absolute.java
@@ -0,0 +1,6 @@
+public class absolute {
+    public static void main(String[] args) {
+        System.out.println("Absolute value of -2: " + Math.abs(-2));
+        System.out.println("Absolute value of 2: " + Math.abs(2));
+    } 
+}
