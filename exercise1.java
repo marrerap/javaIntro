d035c7e97b0873284059ca9a932bac0b2c5d5e02
marrerap <andrew.marrero@outlook.com>
2021-12-22 13:51:18 -0600

    added an exercise and operators file

---
+++ exercise1.java
@@ -0,0 +1,19 @@
+public class exercise1 {
+    public static void main(String[] args) {
+        int int_number = 1000;
+        float float_number = 10.292f;
+        double double_number = 0.00000000000512365123;
+        char char_name = 'N';
+        boolean bool_accept = true;
+
+        System.out.println("Value of Int is: " + int_number);
+
+        System.out.println("Value of Float is: " + float_number);
+
+        System.out.println("Value of Double is: " + double_number);
+
+        System.out.println("Value of Char is: " + char_name);
+
+        System.out.println("Value of Bool is: " + bool_accept);
+    }
+}
---
+++ take_input.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class take_input {
+    public static void main(String[] args) {
+        Scanner scanner_one = new Scanner(System.in);
+
+        System.out.println("Enter your name: ");
+        String name = scanner_one.nextLine();
+        System.out.println("Your name is: " + name);
+    }    
+}
