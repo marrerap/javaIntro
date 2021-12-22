e94e559861db84ce5ef7815e860ca2d59b724c02
marrerap <andrew.marrero@outlook.com>
2021-12-22 14:32:10 -0600

    added min and max math method example

---
+++ max_min.java
@@ -0,0 +1,6 @@
+public class max_min {
+    public static void main(String[] args) {
+        System.out.println("Maximum between 2.04 and 2.05: " + Math.max(2.04, 2.05));
+        System.out.println("Minimum between 2 and 23:" + Math.min(2, 23));
+    }
+}
