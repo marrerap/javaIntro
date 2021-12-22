920710c3eb585b5a27cb38f52fa99b2ef922c05f
marrerap <andrew.marrero@outlook.com>
2021-12-22 12:26:54 -0600

    added multi-line initializations

---
+++ multi_line_initial.java
@@ -0,0 +1,14 @@
+public class multi_line_initial {
+    public static void main(String[] args) {
+        int age = 23, height = 5;
+        char letter = 'A', alpha = 'L';
+        double radius = 2.34, area = 4.55;
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(letter);
+        System.out.println(alpha);
+        System.out.println(radius);
+        System.out.println(area);
+        
+    }
+}
