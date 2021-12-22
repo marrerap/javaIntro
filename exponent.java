80f145d17a976af08c8520c42026505583a7c14f
marrerap <andrew.marrero@outlook.com>
2021-12-22 14:06:46 -0600

    added exponent math methods

---
+++ exponent.java
@@ -0,0 +1,12 @@
+public class exponent {
+    public static void main(String[] args) {
+        // pow takes two sets of numbers and raises the first value to the power of the second value
+        System.out.println("2 raised to the power 3 is " + Math.pow(2, 3));
+        // exp takes one argument and returns the value of e raised to the power of the given argument
+        System.out.println("Exponent squared is " + Math.exp(2));
+        // sqrt gives the square root of the given argument 
+        System.out.println("The square root of 16 is " + Math.sqrt(16));
+        // cbrt gives the cubed root of the given argument
+        System.out.println("The cube root of 27 is " + Math.cbrt(27));
+    }
+}
