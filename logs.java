af6f4f3ec4ff529ab846ff66a878441da3052634
marrerap <andrew.marrero@outlook.com>
2021-12-22 14:14:07 -0600

    added log example for math method

---
+++ logs.java
@@ -0,0 +1,6 @@
+public class logs {
+    public static void main(String[] ars) {
+        System.out.println("log of 2 is " + Math.log(2));
+        System.out.println("lof to the base 10 of 100 is " + Math.log10(100));
+    } 
+}
