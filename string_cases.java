07ef274ce94d2396b6d41b5a8c39c97a65fdf1ae
marrerap <andrew.marrero@outlook.com>
2021-12-27 14:57:37 -0600

    added cases example

---
+++ string_cases.java
@@ -0,0 +1,9 @@
+public class string_cases {
+    public static void main(String[] args) {
+        String greet = "HeLlo WoRld";
+
+        System.out.println(greet.toUpperCase());
+        System.out.println(greet.toLowerCase());
+        
+    }
+}
