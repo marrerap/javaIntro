9968fd34b2aa4f419f05e524e6f6973f27bdf54d
marrerap <andrew.marrero@outlook.com>
2021-12-27 14:46:21 -0600

    added split string example

---
+++ split_string.java
@@ -0,0 +1,10 @@
+public class split_string {
+    public static void main(String[] args)  {
+        String greet = "Hello World,My name is Waldo,How are you?";
+        String[] greetings = greet.split(",");
+        System.out.println(greetings[0]);
+        System.out.println(greetings[1]);
+        System.out.println(greetings[2]);
+
+    }
+}
