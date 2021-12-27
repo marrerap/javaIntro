53871831eab200a9c5e34a48c5842df9471a0a26
marrerap <andrew.marrero@outlook.com>
2021-12-27 15:09:40 -0600

    added length example

---
+++ string_length.java
@@ -0,0 +1,6 @@
+public class string_length {
+    public static void main(String[] args) {
+        String greeting = "Hello World";
+        System.out.println("The length of greeting is " + greeting.length());
+    }    
+}
