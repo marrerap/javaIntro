73ffd86f65aafa40bb61c3d8b7a9105e148335a1
marrerap <andrew.marrero@outlook.com>
2021-12-27 14:25:30 -0600

    added equals string method example

---
+++ concatEquals.java
@@ -0,0 +1,12 @@
+public class concatEquals {
+    public static void main(String[] args) {
+        String one = "Hello";
+        String two = "World";
+        String lower = "hello";
+        String same = "Hello";
+
+        System.out.println(one.equals(two));
+        System.out.println(one.equals(lower));
+        System.out.println(one.equals(same));
+    }
+}
