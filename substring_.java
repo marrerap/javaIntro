a5a8e5b400c3138d8e11a6fe1f71560056232da1
marrerap <andrew.marrero@outlook.com>
2021-12-27 14:53:22 -0600

    added substring example

---
+++ substring_.java
@@ -0,0 +1,11 @@
+public class substring_ {
+    public static void main(String[] args) {
+        String choice = "CoffeeOrTea";
+
+        // first: only one argument 
+        System.out.println(choice.substring(8));
+
+        // second: two arguments
+        System.out.println(choice.substring(0, 6));
+    }
+}
