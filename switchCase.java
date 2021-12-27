4685c33f1a9218cc3ad5ff9d82d9a59c70c58230
marrerap <andrew.marrero@outlook.com>
2021-12-27 16:12:33 -0600

    added swtch case example

---
+++ switchCase.java
@@ -0,0 +1,22 @@
+public class switchCase {
+    public static void main(String[] args) {
+        int x = 2;
+        int temp;
+
+        switch (x) {
+            case 1:
+            case 2:
+            case 3:
+                temp = 0;
+                break;
+            case 4:
+                temp = 4;
+                break;
+        
+            default:
+                temp = 5;
+                break;
+        }
+        System.out.println("Value of temp: " + temp);
+    }
+}
