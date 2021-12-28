dded904a2d916743a52925040ce9204b26c8bda4
marrerap <andrew.marrero@outlook.com>
2021-12-28 09:54:43 -0600

    added exercise for using conditional expression

---
+++ exercise5.java
@@ -0,0 +1,13 @@
+public class exercise5 {
+    public static String evenodd(int x) {
+        String answer = "";
+        
+        
+        
+        
+        answer = ((x % 2) == 0) ?  "even" : "odd" ;
+
+
+        return answer;
+    }
+}
