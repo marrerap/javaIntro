219af67590df3e4f9afbc6bd14b70cdc17b33ab9
marrerap <andrew.marrero@outlook.com>
2021-12-28 11:18:54 -0600

    added do while example

---
+++ whileDo.java
@@ -0,0 +1,10 @@
+public class whileDo {
+    public static void main(String[] args) {
+        int number = 5;
+
+        do {
+            System.out.println("Value of number is: " + number);
+            number++;
+        } while (number <= 9);
+    }
+}
