0fbbb29b32795958caee98431d02a496cb2d33d5
marrerap <andrew.marrero@outlook.com>
2021-12-28 11:02:06 -0600

    added swtich exercise

---
+++ weekdayExercise.java
@@ -0,0 +1,37 @@
+public class weekdayExercise {
+    public static String week_day(int x){
+        String answer = "";
+        String day;
+
+        switch (x) {
+            case 1:
+                day = "Monday";
+                break;
+            case 2:
+                day = "Tuesday";
+                break;
+            case 3:
+                day = "Wednesday";
+                break;
+            case 4:
+                day = "Thursday";
+                break;
+            case 5:
+                day = "Friday";
+                break;
+            case 6:
+                day = "Saturday";
+                break;
+            case 7:
+                day = "Sunday";
+                break;
+            default:
+                day = "invalid input";
+                
+        }
+        answer = day;
+
+
+        return answer;
+    }
+}
