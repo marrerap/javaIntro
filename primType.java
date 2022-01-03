2cd0971e3edd63ad6f2122762c61e08445125860
marrerap <andrew.marrero@outlook.com>
2022-01-03 08:19:22 -0600

    added primitive type example

---
+++ primType.java
@@ -0,0 +1,15 @@
+public class primType {
+    public static void main(String[] args) {
+
+        String name = "John";
+        changeName(name);
+        System.out.print(name);
+    }
+
+    public static void changeName(String n) {
+
+        n = "Doe";
+        System.out.println(n);
+
+    }
+}
