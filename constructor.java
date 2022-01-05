4116a0f11db2fca3d560154d895e469aa162a8e1
marrerap <andrew.marrero@outlook.com>
2022-01-05 12:42:31 -0600

    added constructor example

---
+++ constructor.java
@@ -0,0 +1,25 @@
+public class constructor {
+
+    private String model;
+    private int horsepower;
+
+
+    public constructor(String m, int h) {
+        this.model = m;
+        this.horsepower = h;
+    }
+
+    public void showDetails() {
+        System.out.println("Car horsepower is: " + this.horsepower);
+    }
+
+    class check {
+        public void main(String[] args) {
+            
+        constructor newCar = new constructor("New", 1000);
+
+        newCar.showDetails();
+        }
+    }
+
+}
